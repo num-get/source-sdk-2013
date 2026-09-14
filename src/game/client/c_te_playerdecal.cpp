80fb0203798f1dce08e7ab1bc94b05db00a85fe1
TheNotoriousFrogposter <anotherthrowawayemail101@gmail.com>
2026-09-14 09:20:35 +0800

    Enable sprays

--- src/game/client/c_te_playerdecal.cpp
+++ src/game/client/c_te_playerdecal.cpp
@@ -22,11 +22,7 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-#ifdef TF_CLIENT_DLL
-static ConVar cl_spraydisable( "cl_spraydisable", "1", FCVAR_CLIENTDLL | FCVAR_ARCHIVE, "Disable player sprays." );
-#else
 static ConVar cl_spraydisable( "cl_spraydisable", "0", FCVAR_CLIENTDLL | FCVAR_ARCHIVE, "Disable player sprays." );
-#endif
 
 #ifndef _XBOX
 CLIENTEFFECT_REGISTER_BEGIN( PrecachePlayerDecal )
